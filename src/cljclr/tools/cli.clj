7a278618cc9ed278260bbbde5ae11bca73ef427a
Rob Rowe <rippinrobr@hotmail.com>
2012-02-13 13:01:56 -0500

    ported code with tests'

---
+++ src/cljclr/tools/cli.clj
@@ -0,0 +1,115 @@
+(ns ^{:author "Gareth Jones, modified for ClojureCLR by Rob Rowe"}
+  cljclr.tools.cli
+  (:use [clojure.string :only (replace)]
+        [clojure.pprint :only (pprint cl-format)])
+  (:refer-clojure :exclude [replace]))
+
+(defn build-doc [{:keys [switches docs default]}]
+  [(apply str (interpose ", " switches))
+   (or (str default) "")
+   (or docs "")])
+
+(defn banner-for [specs]
+  (println "Usage:")
+  (println)
+  (let [docs (into (map build-doc specs)
+                   [["--------" "-------" "----"]
+                    ["Switches" "Default" "Desc"]])
+        max-cols (->> (for [d docs] (map count d))
+                      (apply map (fn [& c] (apply vector c)))
+                      (map #(apply max %)))
+        vs (for [d docs]
+             (mapcat (fn [& x] (apply vector x)) max-cols d))]
+    (doseq [v vs]
+      (cl-format true "~{ ~vA  ~vA  ~vA ~}" v)
+      (prn))))
+
+(defn name-for [k]
+  (replace k #"^--no-|^--\[no-\]|^--|^-" ""))
+
+(defn flag-for [^String v]
+  (not (.StartsWith v "--no-")))  ;RR: .startsWith
+
+(defn opt? [^String x]
+  (.StartsWith (str x) "-"))            ;RR: .startsWith and (str x))
+
+(defn flag? [^String x]
+  (.StartsWith x "--[no-]"))      ;RR: .startsWith
+
+(defn end-of-args? [x]
+  (= "--" x))
+
+(defn spec-for
+  [arg specs]
+  (->> specs
+       (filter (fn [s]
+                   (let [switches (set (s :switches))]
+                     (contains? switches arg))))
+       first))
+
+(defn default-values-for
+  [specs]
+  (into {} (for [s specs] [(s :name) (s :default)])))
+
+(defn apply-specs
+  [specs args]
+  (loop [options    (default-values-for specs)
+         extra-args []
+         args       args]
+    (if-not (seq args)
+      [options extra-args]
+      (let [opt  (first args)
+            spec (spec-for opt specs)]
+        (cond
+         (end-of-args? opt)
+         (recur options (into extra-args (vec (rest args))) nil)
+
+         (and (opt? opt) (nil? spec))
+         (throw (Exception. (str "'" opt "' is not a valid argument")))
+         
+         (and (opt? opt) (spec :flag))
+         (recur (assoc options (spec :name) (flag-for opt))
+                extra-args
+                (rest args))
+
+         (opt? opt)
+         (recur (assoc options (spec :name) ((spec :parse-fn) (second args)))
+                extra-args
+                (drop 2 args))
+
+         :default
+         (recur options (conj extra-args (first args)) (rest args)))))))
+
+(defn switches-for
+  [switches flag]
+  (-> (for [^String s switches]
+        (cond
+         (and flag (flag? s))            [(replace s #"\[no-\]" "no-") (replace s #"\[no-\]" "")]
+         (and flag (.StartsWith s "--")) [(replace s #"--" "--no-") s]                            ;RR: .startsWith
+         :default                        [s]))
+      flatten))
+
+(defn generate-spec
+  [raw-spec]
+  (let [[switches raw-spec] (split-with #(and (string? %) (opt? %)) raw-spec)
+        [docs raw-spec]     (split-with string? raw-spec)
+        options             (apply hash-map raw-spec)
+        aliases             (map name-for switches)
+        flag                (or (flag? (last switches)) (options :flag))]
+    (merge {:switches (switches-for switches flag)
+            :docs     (first docs)
+            :aliases  (set aliases)
+            :name     (keyword (last aliases))
+            :parse-fn identity
+            :default  (if flag false nil)
+            :flag     flag}
+           options)))
+
+(defn cli
+  [args & specs]
+  
+  (let [specs (map generate-spec specs)]
+    (let [[options extra-args] (apply-specs specs args)
+          banner  (with-out-str (banner-for specs))]
+      [options extra-args banner])))
+
---
+++ test/cljclr/tools/cli_test.clj
@@ -0,0 +1,95 @@
+(ns cljclr.tools.cli-test
+  (:use [clojure.test]
+        [cljclr.tools.cli]))
+
+(testing "syntax"
+  (deftest should-handle-simple-strings
+    (is (= {:host "localhost"}
+           (first (cli ["--host" "localhost"]
+                       ["--host"])))))
+
+  (testing "booleans"
+    (deftest should-handle-trues
+      (is (= {:verbose true}
+             (first (cli ["--verbose"]
+			 ["--[no-]verbose"])))))
+    
+    (deftest should-handle-falses
+      (is (= {:verbose false}
+             (first (cli ["--no-verbose"]
+                         ["--[no-]verbose"])))))
+    
+    (testing "explicit syntax"
+      (is (= {:verbose true}
+             (first (cli ["--verbose"]
+                         ["--verbose" :flag true]))))
+      (is (= {:verbose false}
+             (first (cli ["--no-verbose"]
+                         ["--verbose" :flag true]))))))
+
+  (testing "default values"
+    (deftest should-default-when-no-value
+      (is (= {:server "10.0.1.10"}
+             (first (cli []
+                         ["--server" :default "10.0.1.10"])))))
+    
+    (deftest should-override-when-supplied
+      (is (= {:server "127.0.0.1"}
+             (first (cli ["--server" "127.0.0.1"]
+                         ["--server" :default "10.0.1.10"])))))
+  (deftest should-apply-parse-fn
+    (is (= {:names ["john" "jeff" "steve"]}
+           (first (cli ["--names" "john,jeff,steve"]
+                       ["--names" :parse-fn #(vec (.Split % (first ",")))])))))) ;RR: (.split % ",") 
+
+  (testing "aliases"
+    (deftest should-support-multiple-aliases
+      (is (= {:server "localhost"}
+             (first (cli ["-s" "localhost"]
+                         ["-s" "--server"])))))
+
+    (deftest should-use-last-alias-provided-as-name-in-map
+      (is (= {:server "localhost"}
+             (first (cli ["-s" "localhost"]
+                        ["-s" "--server"]))))))
+
+  (testing "extra arguments"
+    (deftest should-provide-access-to-trailing-args
+      (let [[options args _] (cli ["--foo" "bar" "a" "b" "c"]
+                                  ["-f" "--foo"])]
+        (is (= {:foo "bar"} options))
+        (is (= ["a" "b" "c"] args))))
+
+    (deftest should-work-with-trailing-boolean-args
+      (let [[options args _] (cli ["--no-verbose" "some-file"]
+                                  ["--[no-]verbose"])]
+        (is (= {:verbose false}))
+        (is (= ["some-file"] args))))
+
+    (deftest should-accept-double-hyphen-as-end-of-args
+      (let [[options args _] (cli ["--foo" "bar" "--verbose" "--" "file" "-x" "other"]
+                                  ["--foo"]
+                                  ["--[no-]verbose"])]
+        (is (= {:foo "bar" :verbose true} options))
+        (is (= ["file" "-x" "other"] args))))))
+
+(deftest all-together-now
+  (let [[options args _] (cli ["-p" "8080"
+                               "--no-verbose"
+                               "--log-directory" "/tmp"
+                               "--server" "localhost"
+                               "filename"]
+                              ["-p" "--port" :parse-fn #(Convert/ToInt32 %)]     ;RR: Integer.
+                              ["--host" :default "localhost"]
+                              ["--[no-]verbose" :default true]
+                              ["--log-directory" :default "/some/path"]
+                              ["--server"])]
+    (is (= {:port 8080
+            :host "localhost"
+            :verbose false
+            :log-directory "/tmp"
+            :server "localhost"} options))
+    (is (= ["filename"] args))))
+
+(deftest clr-specific
+  (is (= true (opt? (first "-")))))
